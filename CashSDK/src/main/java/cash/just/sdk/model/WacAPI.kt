d9939963cd6f7d51d0e2333ea067e8d1e66f4dfb
dabitdev <imdavidfernandez@gmail.com>
2020-08-20 17:05:57 -0400

    changing ATM to WAC

--- CashSDK/src/main/java/cash/just/sdk/model/WacAPI.kt
+++ CashSDK/src/main/java/cash/just/sdk/model/WacAPI.kt
@@ -4,29 +4,29 @@ import retrofit2.Call
 import retrofit2.http.*
 
 interface WacAPI {
-    @POST("/atm/wac/guest/login")
+    @POST("/wac/wac/guest/login")
     fun login(): Call<LoginResponse>
 
-    @GET("/atm/wac/atm/list")
+    @GET("/wac/wac/atm/list")
     fun getAtmList(@Header("sessionKey") sessionKey: String): Call<AtmListResponse>
 
-    @GET("/atm/wac/atm/near/latlon/{lat}/{lon}")
+    @GET("/wac/wac/atm/near/latlon/{lat}/{lon}")
     fun getAtmListByLocation(
         @Header("sessionKey") sessionKey: String,
         @Path(value="lat", encoded=true) lat:String,
         @Path(value="lon", encoded=true) lon:String): Call<AtmListResponse>
 
-    @GET("/atm/wac/pcode/{pcode}")
+    @GET("/wac/pcode/{pcode}")
     fun checkCodeStatus(@Path(value="pcode", encoded=true) code:String, @Header("sessionKey") sessionKey: String): Call<CashCodeStatusResponse>
 
-    @POST("/atm/wac/pcode")
+    @POST("/wac/pcode")
     fun createCode(
         @Header("sessionKey") sessionKey: String,
         @Query(value="atm_id", encoded=true) atmId:String,
         @Query(value="amount", encoded=true) amount:String,
         @Query(value="verification_code", encoded=true) verificationCode:String): Call<CashCodeResponse>
 
-    @POST("/atm/wac/pcode/verify")
+    @POST("/wac/wac/pcode/verify")
     fun sendVerificationCode(
         @Header("sessionKey") sessionKey: String,
         @Query(value="first_name", encoded=true) firstName:String,
